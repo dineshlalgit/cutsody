5315d42e5ccd6240c3295155e09809142b7a1865
Dinesh Lal <dineshlal29@gmail.com>
2023-02-03 22:31:10 +0530

    update logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled from "styled-components";
-import logo from "assets/logo.png";
+import logo from "assets/cutstdio.png";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdClose } from "react-icons/md";
 import { useScroll } from "components/useScroll";
