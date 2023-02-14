8813c069ed01a28e4e94367a3f747ecd78ae4012
im-paul-mak33 <souravpaul210789@gmail.com>
2023-02-14 15:37:09 +0530

    only data and images to be updated

--- src/components/Mens.jsx
+++ src/components/Mens.jsx
@@ -1,13 +1,188 @@
 import React from 'react';
+import styled from 'styled-components';
 import mens from '../data/mens';
 import MensCover from './MensCover';
+import Title from './Title';
+import { useScroll } from './useScroll';
 
 const mensData = mens.map((mens, index) => {
   return <MensCover key={index} {...mens} />;
 });
 
 const Mens = () => {
-  return <div className='flex flex-wrap items-center justify-center mx-auto p-auto font-normal text-lg font-Nunito_Sans py-10'>{mensData}</div>;
+  const [element, controls] = useScroll();
+
+  return (
+    <>
+      <Section ref={element} id="mens">
+        <Title value="Mens" />
+        <div className="background"></div>
+        <div className="pricing__title">
+          <p>Find your hair needs 😁</p>
+          <h2 className="font-Roboto_Condensed font-medium">
+            Finding the best salon service in your city could be time
+            consuming... 😪
+          </h2>
+          <h4 className="text-md font-Roboto_Condensed font-bold text-pink-600">
+            We have you covered 🥳✌🏻{' '}
+          </h4>
+        </div>
+        <div className="p-auto mx-40 flex flex-wrap items-center justify-center gap-10 py-10 font-Roboto_Condensed text-lg font-normal">
+          {mensData}
+        </div>
+      </Section>
+    </>
+  );
 };
+
+const Section = styled.section`
+  background-color: #0093e9;
+  background-image: linear-gradient(160deg, #0093e9 0%, #80d0c7 100%);
+  min-height: 100vh;
+  padding: 3rem 0.5rem;
+  position: relative;
+  overflow: hidden;
+  .background {
+    .bg1 {
+      position: absolute;
+      top: -60%;
+      left: -5%;
+      z-index: -1;
+    }
+  }
+  .pricing__title {
+    margin: 3rem 13rem;
+    p {
+      color: #dc1c1c;
+      text-transform: uppercase;
+      letter-spacing: 0.2rem;
+    }
+    h2 {
+      color: var(--primary-color);
+      font-size: 2rem;
+    }
+  }
+  .pricing {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    margin: 5rem;
+    padding: 0 6rem;
+    &__plan {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 2rem;
+      &:nth-child(2) {
+        .pricing__plan__content {
+          padding: 0rem;
+          border-left: 0.2rem gold;
+          border-right: 0.2rem gold;
+        }
+      }
+      &__name {
+        background-color: black;
+        width: 15rem;
+        height: 15rem;
+        border-radius: 0.5rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        color: var(--secondary-color);
+        h3 {
+          font-size: 1.4rem;
+          line-height: 2rem;
+        }
+        &__price {
+          color: #dc3333;
+          display: flex;
+          position: relative;
+          p {
+            font-size: 0rem;
+            font-weight: bold;
+          }
+        }
+      }
+      &__content {
+        &__features {
+          list-style-type: none;
+          text-align: center;
+          font-weight: 500;
+          color: gold;
+          display: flex;
+          flex-direction: column;
+          .line {
+            text-decoration: line-through;
+          }
+        }
+        &__actions {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          color: var(--primary-color);
+          gap: 0.5rem;
+          span {
+            text-transform: uppercase;
+          }
+        }
+      }
+    }
+  }
+
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+    padding: 1rem;
+    background-color: whitesmoke;
+    .pricing__title {
+      margin: 1rem;
+      padding: 0 2rem;
+      text-align: center;
+      h2 {
+        font-size: 1.3rem;
+      }
+    }
+    .background {
+      display: none;
+    }
+    .pricing {
+      grid-template-columns: 1fr;
+      padding: 1.5rem;
+      gap: -100rem;
+      &__plan {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 1.3rem;
+        &__name {
+          height: 12rem;
+          width: 12rem;
+          h2 {
+            font-size: 1.1rem;
+            color: gold;
+          }
+          h3 {
+            font-size: 1.3rem;
+          }
+          &__price {
+            p {
+              font-size: 1rem;
+            }
+          }
+        }
+        &__content {
+          &__features {
+            color: white;
+          }
+        }
+        &:nth-child(2) {
+          .pricing__plan__content {
+            padding: 0rem;
+            border: none;
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default Mens;
--- src/components/MensCover.jsx
+++ src/components/MensCover.jsx
@@ -6,21 +6,21 @@ const MensCover = (props) => {
   const [openMensMenuDetails, setOpenMensMenuDetails] = useState(false);
 
   return (
-    <div className='m-2 py-3 p-2 flex flex-col w-96'>
+    <div className="m-2 flex w-60 flex-col p-2 py-3">
       <div
         onClick={() => {
           setOpenMensMenuDetails(!openMensMenuDetails);
         }}
-        className='max-w-96 items:center justify-center'
+        className="max-w-60 items:center justify-center"
       >
         <LazyLoadImage
           src={props.imageLink}
           alt={props.name}
-          className='flex rounded-md w-96 h-60 drop-shadow-2xl shadow-black shadow-lg transition:origin-center hover:ease-in-out hover:scale-110 transition:duration-1000 transition-transform'
+          className="transition:origin-center transition:duration-1000 flex h-60 w-60 rounded-md shadow-lg shadow-black drop-shadow-2xl transition-transform hover:scale-110 hover:ease-in-out"
         />
       </div>
-      <div className='flex flex-col justify-between m-auto py-3 p-1 text-center'>
-        <p className=''>{props.name}</p>
+      <div className="m-auto flex flex-col justify-between p-1 py-3 text-center">
+        <p className="">{props.name}</p>
         <p>{props.duration}</p>
         <p>{props.description}</p>
       </div>
@@ -29,65 +29,52 @@ const MensCover = (props) => {
       <Modal
         isOpen={openMensMenuDetails}
         onRequestClose={() => setOpenMensMenuDetails(false)}
-        overlayClassName='' // modal-overlay bg-black opacity
-        className='' // modal-content w-auto h-auto
+        overlayClassName="" // modal-overlay bg-black opacity
+        className="" // modal-content w-auto h-auto
       >
-        <div className='flex flex-col w-auto h-auto font-Nunito_Sans bg-slate-50 shadow-2xl shadow-slate-200 my-10'>
-          <div className='mx-auto'>
-            <span className='text-3xl font-semibold text-slate-900'>
-              Package Details
+        <div className="my-10 flex h-auto w-auto flex-col bg-slate-50 font-Nunito_Sans shadow-2xl shadow-slate-200">
+          <div className="mx-auto">
+            <span className="text-3xl font-semibold text-slate-900">
+              Category Details
             </span>
           </div>
-          <div className='flex flex-row justify-between py-3 mx-4'>
-            <span className='text-2xl text- font-semibold text-black'>
+          <div className="mx-4 flex flex-row justify-between py-3">
+            <span className="text- text-2xl font-semibold text-black">
               {props.name}
             </span>
             <button
-              className='px-2 h-10 font-medium font-Nunito_Sans text-slate-400 hover:text-slate-800 text-2xl border-solid border-2'
+              className="h-10 border-2 border-solid px-2 font-Nunito_Sans text-2xl font-medium text-slate-400 hover:text-slate-800"
               onClick={() => setOpenMensMenuDetails(false)}
             >
               X
             </button>
           </div>
-          <div className='flex flex-row pb-2'>
-            <button className='mx-auto px-2 h-10 font-semibold font-Nunito_Sans text-white bg-gradient-to-br rounded-md to-amber-400 from-pink-500 hover:bg-gradient-to-br hover:to-amber-600 hover:from-pink-600 hover:text-white'>
-              Book Now
-            </button>
-          </div>
-          <div className='py-2'>
-            <span className='text-slate-700 text-base'>
-              {props.value1}
-            </span>
+          <div className="py-2">
+            <span className="text-base text-slate-700">{props.value1}</span>
             {/* <LazyLoadImage
               className='h-80 w-1/3 lg:w-5/12 airbnbml:w-2/3 airbnbml:mx-auto sm:h-52 sm:w-auto'
               src={props.imageLink1 ? props.imageLink1 : '-'}
               alt='.'
             /> */}
           </div>
-          <div className='py-3'>
-            <span className='text-slate-700 text-base'>
-              {props.value2}
-            </span>
+          <div className="py-3">
+            <span className="text-base text-slate-700">{props.value2}</span>
             {/* <LazyLoadImage
               className='h-80 w-1/3 lg:w-5/12 airbnbml:w-2/3 airbnbml:mx-auto sm:h-40'
               src={props.imageLink2 ? props.imageLink2 : '-'}
               alt='.'
             /> */}
           </div>
-          <div className='py-3'>
-            <span className='text-slate-700 text-base'>
-              {props.value3}
-            </span>
+          <div className="py-3">
+            <span className="text-base text-slate-700">{props.value3}</span>
             {/* <LazyLoadImage
               className='h-80 w-1/3 lg:w-5/12 airbnbml:w-2/3 airbnbml:mx-auto sm:h-40'
               src={props.imageLink3 ? props.imageLink3 : '-'}
               alt='.'
             /> */}
           </div>
-          <div className='py-3'>
-            <span className='text-slate-700 text-base'>
-              {props.value4}
-            </span>
+          <div className="py-3">
+            <span className="text-base text-slate-700">{props.value4}</span>
             {/* <LazyLoadImage
               className='h-80 w-1/3 lg:w-5/12 airbnbml:w-2/3 airbnbml:mx-auto sm:h-40'
               src={props.imageLink4 ? props.imageLink4 : '-'}
@@ -95,10 +82,8 @@ const MensCover = (props) => {
             /> */}
           </div>
           {props.value5 ? (
-            <div className='py-3'>
-              <span className='text-slate-700 text-base'>
-                {props.value5}
-              </span>
+            <div className="py-3">
+              <span className="text-base text-slate-700">{props.value5}</span>
               {/* <LazyLoadImage
                 className='h-80 w-1/3 lg:w-5/12 airbnbml:w-2/3 airbnbml:mx-auto sm:h-40'
                 src={props.imageLink5 ? props.imageLink5 : '-'}
@@ -109,10 +94,8 @@ const MensCover = (props) => {
             <div></div>
           )}
           {props.value6 ? (
-            <div className='py-3'>
-              <span className='text-slate-700 text-base'>
-                {props.value6}
-              </span>
+            <div className="py-3">
+              <span className="text-base text-slate-700">{props.value6}</span>
               {/* <LazyLoadImage
                 className='h-80 w-1/3 lg:w-5/12 airbnbml:w-2/3 airbnbml:mx-auto sm:h-40'
                 src={props.imageLink6 ? props.imageLink6 : '-'}
@@ -123,120 +106,104 @@ const MensCover = (props) => {
             <div></div>
           )}
           {props.value7 ? (
-            <div className='py-3'>
-              <span className='text-slate-700 text-base'>
-                {props.value7}
-              </span>
-              <LazyLoadImage
+            <div className="py-3">
+              <span className="text-base text-slate-700">{props.value7}</span>
+              {/* <LazyLoadImage
                 className='h-80 w-1/3 lg:w-5/12 airbnbml:w-2/3 airbnbml:mx-auto sm:h-40'
                 src={props.imageLink7 ? props.imageLink7 : '-'}
                 alt='.'
-              />
+              /> */}
             </div>
           ) : (
             <div></div>
           )}
           {props.value8 ? (
-            <div className='py-3'>
-              <span className='text-slate-700 text-base'>
-                {props.value8}
-              </span>
-              <LazyLoadImage
+            <div className="py-3">
+              <span className="text-base text-slate-700">{props.value8}</span>
+              {/* <LazyLoadImage
                 className='h-80 w-1/3 lg:w-5/12 airbnbml:w-2/3 airbnbml:mx-auto sm:h-40'
                 src={props.imageLink8 ? props.imageLink8 : '-'}
                 alt='.'
-              />
+              /> */}
             </div>
           ) : (
             <div></div>
           )}
           {props.value9 ? (
-            <div className='py-3'>
-              <span className='text-slate-700 text-base'>
-                {props.value9}
-              </span>
-              <LazyLoadImage
+            <div className="py-3">
+              <span className="text-base text-slate-700">{props.value9}</span>
+              {/* <LazyLoadImage
                 className='h-80 w-1/3 lg:w-5/12 airbnbml:w-2/3 airbnbml:mx-auto sm:h-40'
                 src={props.imageLink9 ? props.imageLink9 : '-'}
                 alt='.'
-              />
+              /> */}
             </div>
           ) : (
             <div></div>
           )}
           {props.value10 ? (
-            <div className='py-3'>
-              <span className='text-slate-700 text-base'>
-                {props.value10}
-              </span>
-              <LazyLoadImage
+            <div className="py-3">
+              <span className="text-base text-slate-700">{props.value10}</span>
+              {/* <LazyLoadImage
                 className='h-80 w-1/3 lg:w-5/12 airbnbml:w-2/3 airbnbml:mx-auto sm:h-40'
                 src={props.imageLink10 ? props.imageLink10 : '-'}
                 alt='.'
-              />
+              /> */}
             </div>
           ) : (
             <div></div>
           )}
           {props.value11 ? (
-            <div className='py-3'>
-              <span className='text-slate-700 text-base'>
-                {props.value11}
-              </span>
-              <LazyLoadImage
+            <div className="py-3">
+              <span className="text-base text-slate-700">{props.value11}</span>
+              {/* <LazyLoadImage
                 className='h-80 w-1/3 lg:w-5/12 airbnbml:w-2/3 airbnbml:mx-auto sm:h-40'
                 src={props.imageLink11 ? props.imageLink11 : '-'}
                 alt='.'
-              />
+              /> */}
             </div>
           ) : (
             <div></div>
           )}
           {props.value12 ? (
-            <div className='py-3'>
-              <span className='text-slate-700 text-base'>
-                {props.value12}
-              </span>
-              <LazyLoadImage
+            <div className="py-3">
+              <span className="text-base text-slate-700">{props.value12}</span>
+              {/* <LazyLoadImage
                 className='h-80 w-1/3 lg:w-5/12 airbnbml:w-2/3 airbnbml:mx-auto sm:h-40'
                 src={props.imageLink12 ? props.imageLink12 : '-'}
                 alt='.'
-              />
+              /> */}
             </div>
           ) : (
             <div></div>
           )}
           {props.value13 ? (
-            <div className='py-3'>
-              <span className='text-slate-700 text-base'>
-                {props.value13}
-              </span>
-              <LazyLoadImage
+            <div className="py-3">
+              <span className="text-base text-slate-700">{props.value13}</span>
+              {/* <LazyLoadImage
                 className='h-80 w-1/3 lg:w-5/12 airbnbml:w-2/3 airbnbml:mx-auto sm:h-40'
                 src={props.imageLink13 ? props.imageLink13 : '-'}
                 alt='.'
-              />
+              /> */}
             </div>
           ) : (
             <div></div>
           )}
           {props.value14 ? (
-            <div className='py-3'>
-              <span className='text-slate-700 text-base'>
-                {props.value14}
-              </span>
-              <LazyLoadImage
+            <div className="py-3">
+              <span className="text-base text-slate-700">{props.value14}</span>
+              {/* <LazyLoadImage
                 className='h-80 w-1/3 lg:w-5/12 airbnbml:w-2/3 airbnbml:mx-auto sm:h-40'
                 src={props.imageLink14 ? props.imageLink14 : '-'}
                 alt='.'
-              />
+              /> */}
             </div>
           ) : (
             <div></div>
           )}
-          <div className='mx-auto pt-10 pb-7'>
+          <div className="mx-auto pt-10 pb-7">
             <button
-              className='h-10 w-20 rounded-lg font-Nunito_Sans uppercase mx-auto border-2 bg-rose-600 text-slate-100 hover:bg-gradient-to-br hover:from-red-600 hover:via-red-500 hover:to-yellow-400'
+              className="mx-auto h-10 w-20 rounded-lg border-2 bg-rose-600 font-Nunito_Sans uppercase text-slate-100 hover:bg-gradient-to-br hover:from-red-600 hover:via-red-500 hover:to-yellow-400"
               onClick={() => setOpenMensMenuDetails(false)}
             >
               Back
